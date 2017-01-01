17f83902e66bef1d9cf14aec3c433e6f73ca59b4
Christian Gripp <mail@core23.de>
2017-01-01 17:17:35 +0000

    Apply fixes from StyleCI

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -23,7 +23,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $node = $treeBuilder->root('core23_last_fm');
+        $node        = $treeBuilder->root('core23_last_fm');
 
         $this->addApiSection($node);
         $this->addHttpClientSection($node);
