dd61ea6632dbfec43fd7f8f860d74d1c2628aa66
core23 <mail@core23.de>
2017-02-26 11:19:45 +0100

    Added unit tests for dependency injection

---
+++ Tests/DependencyInjection/ConfigurationTest.php
@@ -0,0 +1,52 @@
+<?php
+
+/*
+ * (c) Christian Gripp <mail@core23.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Core23\LastFmBundle\Tests\DependencyInjection;
+
+use Core23\LastFmBundle\DependencyInjection\Configuration;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Config\Definition\Processor;
+
+class ConfigurationTest extends TestCase
+{
+    public function testOptions()
+    {
+        $processor = new Processor();
+
+        $config = $processor->processConfiguration(new Configuration(), array(array(
+            'api' => array(
+                'app_id'        => 'foo_id',
+                'shared_secret' => 'bar_secret',
+            ),
+        )));
+
+        $expected = array(
+            'api'          => array(
+                'app_id'        => 'foo_id',
+                'shared_secret' => 'bar_secret',
+                'endpoint'      => 'http://ws.audioscrobbler.com/2.0/',
+                'auth_url'      => 'http://www.last.fm/api/auth/',
+            ),
+            'auth_success' => array(
+                'route'            => null,
+                'route_parameters' => array(),
+            ),
+            'auth_error'   => array(
+                'route'            => null,
+                'route_parameters' => array(),
+            ),
+            'http'         => array(
+                'client'          => 'httplug.client.default',
+                'message_factory' => 'httplug.message_factory.default',
+            ),
+        );
+
+        $this->assertSame($expected, $config);
+    }
+}
---
+++ Tests/DependencyInjection/Core23LastFmExtensionTest.php
@@ -0,0 +1,46 @@
+<?php
+
+/*
+ * (c) Christian Gripp <mail@core23.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Core23\LastFmBundle\Tests\DependencyInjection;
+
+use Core23\LastFmBundle\DependencyInjection\Core23LastFmExtension;
+use Matthias\SymfonyDependencyInjectionTest\PhpUnit\AbstractExtensionTestCase;
+
+class Core23LastFmExtensionTest extends AbstractExtensionTestCase
+{
+    public function testLoadDefault()
+    {
+        $this->load(array(
+            'api' => array(
+                'app_id'        => 'foo_id',
+                'shared_secret' => 'bar_secret',
+            ),
+        ));
+
+        $this->assertContainerBuilderHasParameter('core23.lastfm.auth_success.redirect_route', null);
+        $this->assertContainerBuilderHasParameter('core23.lastfm.auth_success.redirect_route_params', array());
+        $this->assertContainerBuilderHasParameter('core23.lastfm.auth_error.redirect_route', null);
+        $this->assertContainerBuilderHasParameter('core23.lastfm.auth_error.redirect_route_params', array());
+
+        $this->assertContainerBuilderHasParameter('core23.lastfm.api.app_id', 'foo_id');
+        $this->assertContainerBuilderHasParameter('core23.lastfm.api.shared_secret', 'bar_secret');
+        $this->assertContainerBuilderHasParameter('core23.lastfm.api.endpoint', 'http://ws.audioscrobbler.com/2.0/');
+        $this->assertContainerBuilderHasParameter('core23.lastfm.api.auth_url', 'http://www.last.fm/api/auth/');
+
+        $this->assertContainerBuilderHasAlias('core23.lastfm.http.client', 'httplug.client.default');
+        $this->assertContainerBuilderHasAlias('core23.lastfm.http.message_factory', 'httplug.message_factory.default');
+    }
+
+    protected function getContainerExtensions()
+    {
+        return array(
+            new Core23LastFmExtension(),
+        );
+    }
+}
