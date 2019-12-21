nucleos/NucleosMenuBundle

Add integration test

tests/App/AppKernel.php
@@ -1,0 +1,71 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * (c) Christian Gripp <mail@core23.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Core23\MenuBundle\Tests\App;
+
+use Core23\MenuBundle\Core23MenuBundle;
+use Core23\MenuBundle\Tests\App\Controller\TwigTestController;
+use Sonata\BlockBundle\SonataBlockBundle;
+use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
+use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Bundle\TwigBundle\TwigBundle;
+use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Kernel;
+use Symfony\Component\Routing\RouteCollectionBuilder;
+
+final class AppKernel extends Kernel
+{
+    use MicroKernelTrait;
+
+    public function __construct()
+    {
+        parent::__construct('test', false);
+    }
+
+    public function registerBundles()
+    {
+        yield new FrameworkBundle();
+        yield new TwigBundle();
+        yield new SonataBlockBundle();
+        yield new Core23MenuBundle();
+    }
+
+    public function getCacheDir(): string
+    {
+        return $this->getBaseDir().'cache';
+    }
+
+    public function getLogDir(): string
+    {
+        return $this->getBaseDir().'log';
+    }
+
+    public function getProjectDir(): string
+    {
+        return __DIR__;
+    }
+
+    protected function configureRoutes(RouteCollectionBuilder $routes): void
+    {
+        $routes->add('/twig-test', TwigTestController::class);
+    }
+
+    protected function configureContainer(ContainerBuilder $containerBuilder, LoaderInterface $loader): void
+    {
+        $loader->load(__DIR__.'/config/config.yaml');
+    }
+
+    private function getBaseDir(): string
+    {
+        return sys_get_temp_dir().'/app-bundle/var/';
+    }
+}

tests/BundleIntegrationTest.php
@@ -1,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * (c) Christian Gripp <mail@core23.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Core23\MenuBundle\Tests;
+
+use Core23\MenuBundle\Tests\App\AppKernel;
+use PHPUnit\Framework\TestCase;
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
+use Symfony\Component\HttpKernel\Client;
+
+final class BundleIntegrationTest extends TestCase
+{
+    public function testStartup(): void
+    {
+        if (class_exists(KernelBrowser::class)) {
+            $client = new KernelBrowser(new AppKernel());
+        } else {
+            $client = new Client(new AppKernel());
+        }
+
+        $client->request('GET', '/twig-test');
+
+        static::assertSame(200, $client->getResponse()->getStatusCode());
+    }
+}

nucleos/NucleosLastFmBundle

tests/App/AppKernel.php
@@ -1,0 +1,69 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * (c) Christian Gripp <mail@core23.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Core23\LastFmBundle\Tests\App;
+
+use Core23\LastFmBundle\Core23LastFmBundle;
+use Core23\LastFmBundle\Tests\App\Controller\TestController;
+use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
+use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Bundle\TwigBundle\TwigBundle;
+use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Kernel;
+use Symfony\Component\Routing\RouteCollectionBuilder;
+
+final class AppKernel extends Kernel
+{
+    use MicroKernelTrait;
+
+    public function __construct()
+    {
+        parent::__construct('test', false);
+    }
+
+    public function registerBundles()
+    {
+        yield new FrameworkBundle();
+        yield new TwigBundle();
+        yield new Core23LastFmBundle();
+    }
+
+    public function getCacheDir(): string
+    {
+        return $this->getBaseDir().'cache';
+    }
+
+    public function getLogDir(): string
+    {
+        return $this->getBaseDir().'log';
+    }
+
+    public function getProjectDir(): string
+    {
+        return __DIR__;
+    }
+
+    protected function configureRoutes(RouteCollectionBuilder $routes): void
+    {
+        $routes->add('/test', TestController::class);
+    }
+
+    protected function configureContainer(ContainerBuilder $containerBuilder, LoaderInterface $loader): void
+    {
+        $loader->load(__DIR__.'/config/config.yaml');
+    }
+
+    private function getBaseDir(): string
+    {
+        return sys_get_temp_dir().'/app-bundle/var/';
+    }
+}

tests/BundleIntegrationTest.php
@@ -1,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * (c) Christian Gripp <mail@core23.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Core23\LastFmBundle\Tests;
+
+use Core23\LastFmBundle\Tests\App\AppKernel;
+use PHPUnit\Framework\TestCase;
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
+use Symfony\Component\HttpKernel\Client;
+
+final class BundleIntegrationTest extends TestCase
+{
+    public function testStartup(): void
+    {
+        if (class_exists(KernelBrowser::class)) {
+            $client = new KernelBrowser(new AppKernel());
+        } else {
+            $client = new Client(new AppKernel());
+        }
+
+        $client->request('GET', '/test');
+
+        static::assertSame(200, $client->getResponse()->getStatusCode());
+    }
+}