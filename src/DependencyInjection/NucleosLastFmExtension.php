4369846187ef962ff8990c4c627a7995a386a80b
core23 <mail@core23.de>
2024-08-14 17:43:13 +0200

    Fix symfony deprecation

--- src/DependencyInjection/NucleosLastFmExtension.php
+++ src/DependencyInjection/NucleosLastFmExtension.php
@@ -13,8 +13,8 @@ namespace Nucleos\LastFmBundle\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 final class NucleosLastFmExtension extends Extension
 {
