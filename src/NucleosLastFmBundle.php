c2cea1543b91a04e27cc7a512e24e6f4971b2d0f
core23 <mail@core23.de>
2025-02-09 10:53:46 +0100

    Fix phpstan findings

--- src/NucleosLastFmBundle.php
+++ src/NucleosLastFmBundle.php
@@ -17,7 +17,7 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 final class NucleosLastFmBundle extends Bundle
 {
-    public function getContainerExtension(): ?ExtensionInterface
+    public function getContainerExtension(): ExtensionInterface
     {
         if (null === $this->extension) {
             $this->extension = new NucleosLastFmExtension();
